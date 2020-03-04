7eff58810a847108c54da9bf02e4d019501ba45b
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-03-04 17:23:07 +0530

    created build files

---
+++ frontend/build/precache-manifest.10168b027ef6a83e28077890807bae9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487e4f58a15a33e9d4364c8258390cb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9bb31e5cf66ce7b087c",
+    "url": "/static/css/main.f527270e.chunk.css"
+  },
+  {
+    "revision": "80f008de737e2ee1e37d",
+    "url": "/static/js/2.dd1f759d.chunk.js"
+  },
+  {
+    "revision": "3bf6c32fa60fa0f7b41615b984399950",
+    "url": "/static/js/2.dd1f759d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9bb31e5cf66ce7b087c",
+    "url": "/static/js/main.91ca92ad.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
