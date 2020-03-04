6e2db01f6d842b95f5bc28b079e0d2141a8d1f4b
DebPaine <49160622+DebPaine@users.noreply.github.com>
2020-03-04 17:24:34 +0530

    added minor changes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const user = require('./routes/user');
 const auth = require('./routes/auth');
 const posts = require('./routes/posts');
 const profile = require('./routes/profile');
+const path = require('path');
 
 const app = express();
 
@@ -11,12 +12,20 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json('This is the root page'));
-
 app.use('/register', user);
 app.use('/auth', auth);
 app.use('/posts', posts);
 app.use('/profile', profile);
+
+// Serve static assets  in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('frontend/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
